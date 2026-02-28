7ea74fdc1f94120128d8d1ef748ad5d910e62142
Stas Osk <stas-stas.stanislav@mail.ru>
2026-02-28 03:59:37 +0300

    описал роуты в отдельный файл

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import MainLayout from '@layouts/MainLayout/MainLayout';
+import { BrowserRouter } from 'react-router-dom';
+import AppRouter from './router/Router';
 
 import '@styles/main.scss';
 
-import Home from './pages/Home/Home';
-import Login from './pages/Login/Login';
-import About from './pages/About/About';
-import NotFound from './pages/NotFound/NotFound';
-
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path="about" element={<About />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </Router>
-  );
+  return <BrowserRouter>
+    <AppRouter />
+  </BrowserRouter>
 };
 
 export default App;
